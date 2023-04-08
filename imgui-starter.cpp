9a9621626549cd539e142f7ce9c7206bd851ea42
bytesiz3d <abdelrahman.farid@gmail.com>
2023-04-08 07:33:10 +0200

    Add dockspace setup

--- imgui-starter.cpp
+++ imgui-starter.cpp
@@ -1,6 +1,5 @@
 #include <imgui-window/Window.h>
-#include <imgui.h>
-#include <imgui_internal.h>
+#include <imgui-window/Dockspace.h>
 
 int
 main(int argc, char const *argv[])
@@ -9,16 +8,38 @@ main(int argc, char const *argv[])
 		return 1;
 
 	im_window::window_run([] {
-		auto dockspace_id = ImGui::DockSpaceOverViewport(nullptr, ImGuiDockNodeFlags_PassthruCentralNode);
-
-		if (static bool first_dock = true; first_dock)
-		{
-			first_dock = false;
-			ImGui::DockBuilderRemoveNodeChildNodes(dockspace_id);
-			ImGui::DockBuilderDockWindow("Dear ImGui Demo", dockspace_id);
-		}
+		static bool reset_dockspace = true;
+		im_window::dockspace({
+			{"Dear ImGui Demo", 0.33f},
+			{im_window::SPLIT_HORIZONTAL, 0.5f, {
+				{"Middle Top", 0.3f},
+				"Middle Bottom",
+			}},
+			"Rightmost"
+		}, &reset_dockspace);
 
 		ImGui::ShowDemoWindow();
+
+		if (ImGui::Begin("Middle Top"))
+		{
+			if (ImGui::Button("Reset dockspace"))
+				reset_dockspace = true;
+		}
+		ImGui::End();
+
+		if (ImGui::Begin("Middle Bottom"))
+		{
+			if (ImGui::Button("Reset dockspace"))
+				reset_dockspace = true;
+		}
+		ImGui::End();
+
+		if (ImGui::Begin("Rightmost"))
+		{
+			if (ImGui::Button("Reset dockspace"))
+				reset_dockspace = true;
+		}
+		ImGui::End();
 	});
 
 	im_window::window_dispose();
