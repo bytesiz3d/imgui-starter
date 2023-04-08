d92e48342ff5509bcc499ddedf23fa1920cb19c9
bytesiz3d <abdelrahman.farid@gmail.com>
2023-04-08 07:42:09 +0200

    Demonstrate lambda capture support

--- imgui-starter.cpp
+++ imgui-starter.cpp
@@ -7,37 +7,41 @@ main(int argc, char const *argv[])
 	if (im_window::window_init() != 0)
 		return 1;
 
-	im_window::window_run([] {
-		static bool reset_dockspace = true;
-		im_window::dockspace({
+	struct {
+		bool reset_dockspace{true};
+		const im_window::Dockspace_Node layout{
 			{"Dear ImGui Demo", 0.33f},
 			{im_window::SPLIT_HORIZONTAL, 0.5f, {
 				{"Middle Top", 0.3f},
 				"Middle Bottom",
 			}},
 			"Rightmost"
-		}, &reset_dockspace);
+		};
+	} STATE{};
+
+	im_window::window_run([&STATE] {
+		im_window::dockspace(STATE.layout, &STATE.reset_dockspace);
 
 		ImGui::ShowDemoWindow();
 
 		if (ImGui::Begin("Middle Top"))
 		{
 			if (ImGui::Button("Reset dockspace"))
-				reset_dockspace = true;
+				STATE.reset_dockspace = true;
 		}
 		ImGui::End();
 
 		if (ImGui::Begin("Middle Bottom"))
 		{
 			if (ImGui::Button("Reset dockspace"))
-				reset_dockspace = true;
+				STATE.reset_dockspace = true;
 		}
 		ImGui::End();
 
 		if (ImGui::Begin("Rightmost"))
 		{
 			if (ImGui::Button("Reset dockspace"))
-				reset_dockspace = true;
+				STATE.reset_dockspace = true;
 		}
 		ImGui::End();
 	});
